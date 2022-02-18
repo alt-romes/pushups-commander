85a7e89cbd40f93665274161dea728171ee093d1
romes <rodrigo.m.mesquita@gmail.com>
2022-02-18 11:14:45 +0000

    classificações bot

--- pushups-logic/app/Main.hs
+++ pushups-logic/app/Main.hs
@@ -1,25 +1,76 @@
+{-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import qualified Data.Text.IO as TIO (readFile)
-import qualified Data.Text as T (pack, init, Text)
+import qualified Data.Text as T (pack, unpack, init, Text)
 import Data.ByteString (ByteString(..))
 import Data.Text.Encoding (encodeUtf8)
 import Servant (Server, Proxy(..), Handler)
 import Control.Monad.IO.Class (MonadIO)
-import Control.Monad (unless)
+import Control.Monad (unless, (>=>))
 
 import Cob
+import Cob.RecordM
+import Cob.RecordM.TH
 import Bots
 import SlackBot
 import DiscordBot
 import PushupsCommander
 
+
+type CobBot m i o = Bot (CobT m) i o
+
+runCobBot :: Monad m => CobSession -> CobBot m i [ChatBotCommands] -> Bot m i [ChatBotCommands]
+runCobBot session = fmap (either (\s -> [ReplyWith $ T.pack s]) id) . transformBot (runCobT session)
+
+
 echoBot :: ChatBot
 echoBot = Bot handler
     where
-    handler m = if isFromBot m then pure [] else
-        pure [ReactWith "slight_smile", ReplyWith ("Echo: " <> getContent m)]
+    handler m =
+        if isFromBot m
+           then pure []
+           else pure [ReactWith "slight_smile", ReplyWith ("Echo: " <> getContent m)]
+
+adderBot :: ChatBot
+adderBot = Bot $ \m ->
+    if isFromBot m then pure [] else pure
+        [ReplyWith ("Echo: " <> (int2Str . (+1) . str2Int . getContent) m)]
+    where
+    str2Int = read @Int . T.unpack
+    int2Str = T.pack . show @Int
+
+
+
+
+
+
+
+data Classficação = Classficação { thisMonth :: String
+                                 , fullName  :: String }
+mkRecord ''Classficação "CASA Finanças Classificação" ["This Month", "Full Name"]
+
+instance Show Classficação where
+    show (Classficação m f) = f <> ": " <> m <> "\n"
+
+-- cobClassf = Bot (rmDefinitionSearch_ . getContent >=> pure . (:[]) . ReplyWith . T.pack . show @[Classficação])
+cobClassf :: (MonadIO m, ChatBotMessage i) => CobBot m i [ChatBotCommands]
+cobClassf = Bot $ \m -> do
+    classf :: [Classficação] <- rmDefinitionSearch_ (getContent m)
+    pure [ReplyWith (T.pack (show classf))]
+
+
+
+
+
+
+
+
+
 
 
 main :: IO ()
@@ -30,9 +81,6 @@ main = do
     cobToken <- init <$> readFile "cob-token.secret"
     session  <- makeSession host cobToken
     putStrLn "Starting..."
-    runBotServersIO 25564
 
-        ( pushupsBot session )
+    runBotServersIO 25564 (runCobBot session cobClassf) [ slackServer (slackToken, session), discordServer discordToken ]
 
-        [ slackServer (slackToken, session)
-        , discordServer discordToken ]
